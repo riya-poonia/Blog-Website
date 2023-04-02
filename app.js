c1db463dfa83d22e6e9f0c4f73af0cbe35c1ae03
Riya Poonia <riyapoonia@Riyas-MacBook-Air.local>
2023-04-02 23:52:58 +0530

    added compose file

--- app.js
+++ app.js
@@ -37,6 +37,13 @@ app.get("/contact", function (req, res) {
   });
 });
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+app.post("/compose", function (req, res) {
+  console.log(req.body.postTitle);
+});
+
 app.listen(5000, function () {
   console.log("Server started on port 5000");
 });
