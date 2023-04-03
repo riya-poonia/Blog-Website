0a19e905152dcf96fe80e80448b8b05f3421d47c
Riya Poonia <riyapoonia@Riyas-MacBook-Air.local>
2023-04-03 18:40:59 +0530

    Made final changes to app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-//jshint esversion:6
-
+//Setup
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+//Starting content
+const posts = [];
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -12,39 +17,33 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-const posts = [];
-
+//Home
 app.get("/", function (req, res) {
-  //res.render("home");
-  //console.log(posts);
   res.render("home", {
     homeContent: homeStartingContent,
     posts: posts,
   });
 });
+
+//About
 app.get("/about", function (req, res) {
-  //res.render("about");
   res.render("about", {
     aboutContent: aboutContent,
   });
 });
+
+//Contact
 app.get("/contact", function (req, res) {
-  //res.render("contact");
   res.render("contact", {
     contactContent: contactContent,
   });
 });
 
+//Compose
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
 app.post("/compose", function (req, res) {
-  //console.log(req.body.postTitle);
   var post = {
     title: req.body.postTitle,
     content: req.body.postBody,
@@ -53,12 +52,12 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
+//Individual Posts
 app.get("/posts/:postName", function (req, res) {
   var requestedTitle = _.lowerCase(req.params.postName);
   posts.forEach(function (post) {
     var storedTitle = _.lowerCase(post.title);
     if (storedTitle === requestedTitle) {
-      //console.log("Match Found");
       res.render("post", {
         title: post.title,
         content: post.content,
@@ -67,6 +66,7 @@ app.get("/posts/:postName", function (req, res) {
   });
 });
 
+//Server
 app.listen(5000, function () {
   console.log("Server started on port 5000");
 });
